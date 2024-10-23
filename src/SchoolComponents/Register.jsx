f4e7e83935bde87929acf262cd6b5aae7d7bc3f7
Xomcreate <priscaojimba15@gmail.com>
2024-10-23 12:42:53 +0100

    3rd branch

--- src/SchoolComponents/Register.jsx
+++ src/SchoolComponents/Register.jsx
@@ -2,27 +2,26 @@ import React from 'react'
 
 function Register() {
   return (
-    <div className='  h-[100vh] w-[100vw] grid justify-center items-center rounded-lg'>
-      <form action="">
-      <div className=' bg-[#fdfdfd] h-[70vh] w-[50vw] grid grid-rows-6' >
-        <div className=' bg-[#dddcdc] row-span-1 grid justify-center items-center font-mono font-semibold text-[20px]'>Create an account</div>
-        <div className='  row-span-5 grid grid-rows-4' >
-          <div className='  row-span-3 gap-[10px]  grid grid-cols-2'>
-            <div className='  ml-[30px] grid grid-rows-3'>
-              <div className='   grid  items-center font-semibold text-[15px] '>FullName <p><input placeholder=' Your name' className='  bg-gray-500  h-[6vh] w-[20vw]' type="text" /></p></div>
-              <div className='   font-semibold text-[15px] items-center grid'>Phone No: <p><input placeholder=' Your Phone no' className='  bg-gray-500  h-[6vh] w-[20vw]' type="text" /></p></div>
-              <div className=' font-semibold text-[15px] items-center grid'>email <p><input placeholder='Your email address' className='  bg-gray-500  h-[6vh] w-[20vw]' type="email" /></p></div>
-            </div>
-            <div className='  mr-[30px] grid grid-rows-3 '>
-            <div className='   grid  items-center font-semibold text-[15px] '>Password <p><input placeholder='Set Password' className='  h-[6vh]  bg-gray-500 w-[20vw]' type="password" /></p></div>
-              <div className='   font-semibold text-[15px] items-center grid'>Confirm Password <p><input placeholder=' Retype password' className='  bg-gray-500 h-[6vh] w-[20vw]' type="password" /></p></div>
-              <div className=' font-semibold text-[15px] items-center grid'>state: <p><input placeholder=' Your state' className=' bg-gray-500  h-[6vh] w-[20vw]' type="text" /></p></div>
-            </div>
-          </div>
-          <div className=' bg-[wheat row-span-1] grid justify-center items-center'><button className=' h-[6vh] bg-[orangered] text-white font-bold rounded-lg w-[10vw]'>Register</button></div>
-        </div>
+    <div className='  justify-center grid items-center h-[100vh] w-[100vw] '>
+     <form action="">
+     <div className=' bg-[#f7f2f2] h-[80vh] w-[45vw] grid grid-rows-8 rounded-lg gap-[5px]'>
+    <div className=' bg-[#c4c4c4] grid justify-center items-center font-mono font-semibold text-[20px] row-span-1'>Create an account</div>
+    <div className='  row-span-6 grid ml-[25px] gap-[5px] grid-rows-6'>
+      <div className='  grid items-center text-[15px] font-semibold '>
+        Firstname <p><input type="text" className=' rounded-lg bg-gray-500 h-[6vh] w-[40vw]' placeholder=' Your name' /></p>
       </div>
-      </form>
+      <div className='  grid items-center text-[15px] font-semibold  '> Email <p><input type="email" className=' rounded-lg bg-gray-500 h-[6vh] w-[40vw]' placeholder=' email address' /></p></div>
+      <div className='  grid items-center text-[15px] font-semibold '> Phone No: <p><input type="text" className=' rounded-lg bg-gray-500 h-[6vh] w-[40vw]' placeholder=' Phone number' /></p></div>
+      <div className='  grid items-center text-[15px] font-semibold  '> State <p><input type="text" className=' rounded-lg bg-gray-500 h-[6vh] w-[40vw]' placeholder=' Your state' /></p></div>
+      <div className='  grid items-center text-[15px] font-semibold  '> Password <p><input type="password" className=' rounded-lg bg-gray-500 h-[6vh] w-[40vw]' placeholder=' Set your password' /></p></div>
+      <div className='  grid items-center text-[15px] font-semibold  '> Confirm Password <p><input type="password" className=' rounded-lg bg-gray-500 h-[6vh] w-[40vw]' placeholder=' Re-type password' /></p></div>
+      
+
+    </div>
+    <div className=' grid justify-center items-center row-span-1'><button className=' text-[white] font-semibold bg-[orangered] h-[6vh] rounded-lg w-[10vw]'>Register</button></div>
+
+         </div>
+     </form>
     </div>
   )
 }
