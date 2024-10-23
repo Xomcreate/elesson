5ebca74168ef8120e789717a7e91ef26f85e2648
kingsLib5 <libertykings88@gmail.com>
2024-10-24 00:51:07 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Header from './SchoolComponents/Header'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Hero from './SchoolComponents/Hero'
+import About from './SchoolComponents/About'
+import Pastquestions from './SchoolComponents/Pastquestions'
+import Cbtquestions from './SchoolComponents/Cbtquestions'
+import Contactus from './SchoolComponents/Contactus'
+import Blog from './SchoolComponents/Blog'
+import Login from './SchoolComponents/Login'
 import Register from './SchoolComponents/Register'
-// import Header from './SchoolComponents/Header'
-// import { BrowserRouter,Routes,Route } from 'react-router-dom'
-// import Hero from './SchoolComponents/Hero'
-// import About from './SchoolComponents/About'
-// import Pastquestions from './SchoolComponents/Pastquestions'
-// import Cbtquestions from './SchoolComponents/Cbtquestions'
-// import Contactus from './SchoolComponents/Contactus'
-
-
 
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <BrowserRouter>
+    <BrowserRouter>
     <Header/>
-   
       < Routes>
       <Route path='/' element= {<Hero/>} />
       <Route path='/about' element= {<About/>} />
       <Route path='/cbt' element= {<Cbtquestions/>} />
+      <Route path='/blog' element= {<Blog/>} />
       <Route path='/past' element= {<Pastquestions/>} />
       <Route path='/contact' element= {<Contactus/>} />
       
       </Routes>
-    
-    </BrowserRouter> */}
-
+    </BrowserRouter>
     <Register/>
+    <Login/>
   </StrictMode>,
 )
